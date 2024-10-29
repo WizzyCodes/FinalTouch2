5ba85b819536891866b077c84299c2445466e2ff
WizzyCodes <wisdomiberi832@gmail.com>
2024-10-29 12:38:56 +0100

    change to readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import data from "./data.json";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <marquee behavior="" direction="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ex totam,
+        aperiam quam expedita aliquam, dolorum recusandae minus natus impedit
+        exercitationem. Eaque sit veritatis quia quae exercitationem veniam
+        pariatur dolorem!
+      </marquee>
+      x
+    </>
+  );
 };
 
 export default App;
